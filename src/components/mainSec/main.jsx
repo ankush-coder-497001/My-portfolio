eb6bbf5532e909237b8f9d646c00c1a8c19df5be
Ankush Kumar Gupta <167736782+ankush-coder-497001@users.noreply.github.com>
2024-08-18 16:46:46 +0530

    nothing serious

--- src/components/mainSec/main.jsx
+++ src/components/mainSec/main.jsx
@@ -65,7 +65,7 @@ const Main=({oncontactClick,projects,profileshow})=>{
           Instagram
         </a>
         <a
-          className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-700 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-400 dark:focus-visible:ring-gray-300 handles"
+          className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-700 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-400 dark:focus-visible:ring-gray-300 handles whts-app"
           href="https://wa.link/5o1g3k"
           rel="ugc"
           target="_blank"
