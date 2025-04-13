aa775ee4e5d1a9a7cbf0edee432d5f055c4148ae
Ankush Kumar Gupta <167736782+ankush-coder-497001@users.noreply.github.com>
2025-04-13 21:15:18 +0530

    added new resume

--- src/components/mainSec/main.jsx
+++ src/components/mainSec/main.jsx
@@ -111,7 +111,7 @@ const Main=({oncontactClick,projects,profileshow})=>{
                   <a
                     className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-transparent px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300  cursor-pointer my-hover-but"
                     rel="ugc"
-                    href="https://drive.google.com/file/d/1xDnJZcWK3-bdcjQEcJu4pUffJ8LR6tyw/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1D5WOOPw-wdo1ozgr9ogtRcKTg12zKI7T/view?usp=sharing"
                   >
                    Resume   <RxResume style={{height:'30px',width:'30px'}} />
                   </a>
